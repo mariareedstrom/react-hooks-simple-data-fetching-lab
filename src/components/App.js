0ebdc01747bf5ece4cccd73c59365e80509c456e
Maria Reedstrom <maria.reedstrom@gmail.com>
2022-02-03 16:02:12 -0600

    commit tree 6f412797b349f34f75e7882d8b10491f710a8552

--- src/components/App.js
+++ src/components/App.js
@@ -1,1 +1,25 @@
-// create your App component here
+import React, { useState, useEffect } from "react";
+
+function App() {
+  const [isLoaded, setIsLoaded] = useState(false);
+  const [dogImage, setDogImage] = useState("");
+
+  useEffect(() => {
+    fetch("https://dog.ceo/api/breeds/image/random")
+      .then((resp) => resp.json())
+      .then((image) => {
+        setDogImage(image.message);
+        setIsLoaded(true);
+      });
+  }, []);
+
+  if (!isLoaded) return <p>Loading...</p>;
+
+  return (
+    <div>
+      <img src={dogImage} alt={"A Random Dog"}></img>
+    </div>
+  );
+}
+
+export default App;
